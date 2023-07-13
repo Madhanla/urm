4e94002c26139681ca5231c0320bcf5270a711e3
E9E4124B <buztor@hotmail.com>
2023-07-13 05:06:16 +0200

    Ordenado Data.URM.PRF

--- src/Data/URM/PRF.hs
+++ src/Data/URM/PRF.hs
@@ -15,27 +15,32 @@
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
 
 module Data.URM.PRF
-  ( prfExtToProg,
-    prfToProg,
-    testFormaCanonica,
+  ( -- | Modulo donde defino una forma de pasar de PRFs a URMs basada
+    -- | también en el libro de Cutland
+
+    -- Tipos para pegar programas
+    Pegar(..),
+
+    -- Deslizamientos y formas canónicas
+    testFormaCanonica, deslizamiento,
+
+    -- Conversión PRF a URM
+    prfExtToProg, prfToProg,
+
+    -- Ejemplo: se puede hacer `computaFunc examplePor [4, 6]`
+    examplePor,
   )
 where
 
--- import Data.List.Index (imap)
--- import Data.Foldable (fold)
---import Data.PRF.Funciones
+import Data.PRF.Funciones as F
 import Data.PRF.PRFExt
 import Data.URM
 import qualified Data.Vector as V
 import qualified Data.Vector.Sized as Sized
 
-deslizamiento :: Int -> Int -> Orden -> Orden
-deslizamiento t k = \case
-  Zero m -> Zero (m + k)
-  Suc m -> Suc (m + k)
-  Transfer m n -> Transfer (m + k) (n + k)
-  Jump m n q -> Jump (m + k) (n + k) (q + t)
+-- | Tipos
 
+-- Este tipo me permite pegar programas
 newtype Pegar = Pegar {unPegar :: V.Vector Orden}
   deriving (Eq, Show, Read)
 
@@ -45,8 +50,28 @@ instance Semigroup Pegar where
 instance Monoid Pegar where
   mempty = Pegar mempty
 
+-- | Funciones de ayuda
+
+-- Recorta / anade ceros a un vector hasta que tenga tamano n
 resizeVec' :: Int -> V.Vector Int -> V.Vector Int
 resizeVec' n = V.fromList . take n . (++ repeat 0) . V.toList
+
+-- Desliza t unidades temporales y k espaciales una orden
+deslizamiento :: Int -> Int -> Orden -> Orden
+deslizamiento t k = \case
+  Zero m -> Zero (m + k)
+  Suc m -> Suc (m + k)
+  Transfer m n -> Transfer (m + k) (n + k)
+  Jump m n q -> Jump (m + k) (n + k) (q + t)
+
+-- Comprueba si un programa esta en forma canonica
+testFormaCanonica :: Prog -> Bool
+testFormaCanonica p = (getMaxPos . foldMap (MaxPos . go) $ p) <= s+1
+  where
+    go (Jump _ _ q) = q
+    go _ = 0
+    s = length p
+
 -- Modifica un programa para que trabaje con la lista de registros
 -- dada como entrada y devolviendo el valor en el otro registro
 -- senalado. Esto requiere de rho(p) + len(p) + 1 instrucciones
@@ -60,6 +85,8 @@ trabajaCon ms n p = unPegar $
   Pegar (fromList [Transfer 1 n])
   where
     ms' = resizeVec' (rho p) ms
+
+-- | Conversión PRF a URM
 
 -- Convierte una PRF a un programa en forma estándar
 prfExtToProg ::
@@ -103,6 +130,7 @@ prfExtToProg extToProg = go where
         m = getMaxPos $ (MaxPos (rho p) <> MaxPos (rho p') <> MaxPos (n'+2))
     Mu (go -> p) -> p'     
       where
+        -- La minimalizacion tengo que comprobar que funciona
         p' = (unPegar $
               Pegar (V.fromList (map (\i -> Transfer i (m+i)) [1..n])) <>
               Pegar (trabajaCon (V.fromList . map (+(m+1)) . take (n+1) $ [0..])
@@ -116,14 +144,12 @@ prfExtToProg extToProg = go where
      where
        n = fromIntegral $ natVal (Proxy @m)
   
-
+-- Caso particular de PRF sin extensiones
 prfToProg :: forall n. (KnownNat n) => PRF n -> Prog
 prfToProg = prfExtToProg (\case)
-
-testFormaCanonica :: Prog -> Bool
-testFormaCanonica p = (getMaxPos . foldMap (MaxPos . go) $ p) <= s+1
-  where
-    go (Jump _ _ q) = q
-    go _ = 0
-    s = length p
     
+-- | Ejemplo
+
+-- multiplicacion en URM
+examplePor :: Prog
+examplePor = prfToProg F.por
